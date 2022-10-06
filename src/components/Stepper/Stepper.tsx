db254d8de5b4896e4ac13b7ee993858182552343
David <sanderdj90@gmail.com>
2022-10-05 18:04:22 -0600

    begin updates

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -9,9 +9,18 @@ import Button from '@mui/material/Button'
 import Slider from '@mui/material/Slider'
 import Typography from '@mui/material/Typography'
 import { graphql, useStaticQuery } from 'gatsby'
+import { Grid } from '@mui/material'
 import { steps, stepsLength } from './constants'
 import { ageEstSimple } from './Stepper.utils'
+import './Stepper.css'
 import rFormula from '../../../static/assets/r-formula.png'
+import densityScale from '../../../static/assets/density-scale.png'
+import ubScale from '../../../static/assets/ub-scale.png'
+import lbScale from '../../../static/assets/lb-scale.png'
+import otlScale from '../../../static/assets/otl-scale.png'
+import stScale from '../../../static/assets/st-scale.png'
+import topoScale from '../../../static/assets/topo-scale.png'
+import useWindowWidth from '../../pages/hooks/useWindowWidth'
 
 interface Props {
   isTest: boolean
@@ -47,14 +56,34 @@ function StepperComponent({ isTest }: Props) {
   }
 `)
 
+  const { width } = useWindowWidth()
   const [activeStep, setActiveStep] = useState<ActiveStep>(0)
   const [currentSliderValue, setCurrentSliderValue] = useState<number>(steps[0].marks[1].value)
   const [results, setResults] = useState<number[]>([])
   const [dynamicWeights, setDynamicWeights] = useState(weights)
 
+  const getImageForStep = (step: ActiveStep) => {
+    switch (true) {
+      case step === 0:
+        return densityScale
+      case step === 1:
+        return ubScale
+      case step === 2:
+        return lbScale
+      case step === 3:
+        return otlScale
+      case step === 4:
+        return stScale
+      case step === 5:
+        return topoScale
+      default:
+        return null
+    }
+  }
+
   // TESTING ONLY
   const handleWeightChange = (id: string, value) => {
-    console.log({id, value})
+    console.log({ id, value })
     setDynamicWeights((prev) => ({
       ...prev,
       [id]: parseFloat(value),
@@ -116,9 +145,10 @@ function StepperComponent({ isTest }: Props) {
               <label htmlFor="intercept">
                 intercept
                 <input
-                  onChange={(e) =>{
+                  onChange={(e) => {
                     console.log(e.target)
-                    handleWeightChange(e.target.id, e.target.value)}}
+                    handleWeightChange(e.target.id, e.target.value)
+                  }}
                   type="text"
                   defaultValue={dynamicWeights.intercept}
                   id="intercept"
@@ -230,28 +260,44 @@ function StepperComponent({ isTest }: Props) {
               <Typography variant="caption" sx={{ mx: 2 }}>{results[index] && results[index]}</Typography>
             </StepLabel>
             <StepContent>
-              <Typography>{step.description}</Typography>
-              <Typography variant="caption">
-                Min:
-                {' '}
-                {step.range.min}
-                {' '}
-                | Max:
-                {' '}
-                {step.range.max}
-              </Typography>
-              <Slider
-                size="medium"
-                value={currentSliderValue}
-                min={step.range.min}
-                max={step.range.max}
-                aria-label="Medium"
-                valueLabelDisplay="on"
-                marks={step.marks}
-                onChange={(e) => {
-                  if (typeof e.target.value === 'number') setCurrentSliderValue(e.target.value)
-                }}
-              />
+              <Grid container>
+                <Grid item xs={8}>
+                  {activeStep <= 5 && (
+                  <img
+                    className="scale"
+                    src={getImageForStep(activeStep)}
+                    style={{ width: '100%' }}
+                    alt={step.description}
+                  />
+                  )}
+                  <Box sx={{ mx: width <  1000 ? 0 : activeStep === 0 ? 20 : 5 }}>
+                    <Slider
+                      size="small"
+                      value={currentSliderValue}
+                      {...activeStep === 0 ? { step: 5 } : {}}
+                      min={step.range.min}
+                      max={step.range.max}
+                      aria-label="Medium"
+                      valueLabelDisplay="on"
+                      marks={step.marks}
+                      onChange={(e) => {
+                        if (typeof e.target.value === 'number') setCurrentSliderValue(e.target.value)
+                      }}
+                    />
+                  </Box>
+                  <Typography>{step.description}</Typography>
+                  <Typography variant="caption">
+                    Min:
+                    {' '}
+                    {step.range.min}
+                    {' '}
+                    | Max:
+                    {' '}
+                    {step.range.max}
+                  </Typography>
+
+                </Grid>
+              </Grid>
               <Box sx={{ mb: 2 }}>
                 <div>
                   <Button
--- src/components/Stepper/constants.js
+++ src/components/Stepper/constants.js
@@ -14,8 +14,56 @@ export const defaultMarks = [
     label: '3',
   },
   {
+    value: 4,
+    label: '4',
+  },
+  {
+    value: 5,
+    label: '5',
+  },
+  {
+    value: 6,
+    label: '6',
+  },
+  {
+    value: 7,
+    label: '7',
+  },
+  {
+    value: 8,
+    label: '8',
+  },
+  {
     value: 9,
     label: '9',
+  },
+  {
+    value: 9,
+    label: '9',
+  },
+  {
+    value: 10,
+    label: '10',
+  },
+  {
+    value: 11,
+    label: '11',
+  },
+  {
+    value: 12,
+    label: '12',
+  },
+  {
+    value: 13,
+    label: '13',
+  },
+  {
+    value: 14,
+    label: '14',
+  },
+  {
+    value: 15,
+    label: '15',
   },
   {
     value: 16,
@@ -33,7 +81,7 @@ export const densityMarks = [
     label: '5',
   },
   {
-    value: 16,
+    value: 10,
     label: '10',
   },
 ]
